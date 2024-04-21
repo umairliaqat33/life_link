9b80ffb87526559dcfcd974c6af24bb9e3100279
umairliaqat33 <umairliaqat552@gmail.com>
2024-04-21 22:28:10 +0500

    added file picker in services and doctor adding screen, and in widgets

--- lib/UI/screens/doctor_adding_screen/doctor_adding_screen.dart
+++ lib/UI/screens/doctor_adding_screen/doctor_adding_screen.dart
@@ -1,15 +1,19 @@
 import 'dart:developer';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:life_link/UI/widgets/buttons/custom_button.dart';
 import 'package:life_link/UI/widgets/general_widgets/app_bar_widget.dart';
+import 'package:life_link/UI/widgets/image_pickers/big_image_picker.dart';
 import 'package:life_link/UI/widgets/text_fields/text_form_field_widget.dart';
 import 'package:life_link/config/size_config.dart';
 import 'package:life_link/controllers/firestore_controller.dart';
 import 'package:life_link/models/doctor_model/doctor_model.dart';
-import 'package:life_link/services/services.dart';
+import 'package:life_link/services/date_and_time_service.dart';
+import 'package:life_link/services/media_service.dart';
+import 'package:life_link/utils/enums.dart';
 import 'package:life_link/utils/exceptions.dart';
 import 'package:life_link/utils/utils.dart';
 
@@ -29,7 +33,9 @@ class _DoctorAddingScreenState extends State<DoctorAddingScreen> {
   final TextEditingController _specialityController = TextEditingController();
   final TextEditingController _otherExperienceController =
       TextEditingController();
+
   final _formKey = GlobalKey<FormState>();
+  PlatformFile? _profilePlatformFile;
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +61,17 @@ class _DoctorAddingScreenState extends State<DoctorAddingScreen> {
                     children: [
                       Column(
                         children: [
+                          ImagePickerBigWidget(
+                            heading: 'Profile Photo',
+                            description:
+                                'add a close-up image of yourself max size is 2 MB',
+                            onPressed: () async => _selectProfileImage(),
+                            platformFile: _profilePlatformFile,
+                            imgUrl: null,
+                          ),
+                          SizedBox(
+                            height: SizeConfig.height8(context),
+                          ),
                           TextFormFieldWidget(
                             controller: _nameController,
                             validator: (value) => Utils.nameValidator(value),
@@ -151,7 +168,7 @@ class _DoctorAddingScreenState extends State<DoctorAddingScreen> {
   }
 
   Future<void> _selectTime(TextEditingController controller) async {
-    TimeOfDay? timeOfDay = await Services.timePicker(context);
+    TimeOfDay? timeOfDay = await DateAndTimeService.timePicker(context);
     if (timeOfDay == null) {
       Fluttertoast.showToast(msg: "Please select some time");
     } else {
@@ -168,10 +185,14 @@ class _DoctorAddingScreenState extends State<DoctorAddingScreen> {
     }
   }
 
-  void _uploadDoctorData() {
+  Future<void> _uploadDoctorData() async {
     try {
       FirestoreController firestoreController = FirestoreController();
       if (_formKey.currentState!.validate()) {
+        String? imageLink = await MediaService.uploadFile(
+          userType: UserType.hospital.name,
+          platformFile: _profilePlatformFile,
+        );
         firestoreController.uploadDoctor(
           DoctorModel(
             email: _emailController.text,
@@ -181,6 +202,7 @@ class _DoctorAddingScreenState extends State<DoctorAddingScreen> {
             education: _educationController.text,
             speciality: _specialityController.text,
             otherExperiences: _otherExperienceController.text,
+            profileImage: imageLink!,
           ),
         );
         Fluttertoast.showToast(msg: "Doctor's data uploaded successfully");
@@ -197,6 +219,24 @@ class _DoctorAddingScreenState extends State<DoctorAddingScreen> {
     } on UnknownException catch (e) {
       Fluttertoast.showToast(
         msg: e.message,
+      );
+    }
+  }
+
+  Future<void> _selectProfileImage() async {
+    try {
+      _profilePlatformFile = await MediaService.selectFile();
+      if (_profilePlatformFile != null) {
+        log("Big Image Clicked");
+        log(_profilePlatformFile!.name);
+      } else {
+        log("no file selected");
+        return;
+      }
+      setState(() {});
+    } catch (e) {
+      Fluttertoast.showToast(
+        msg: e.toString(),
       );
     }
   }
---
+++ lib/UI/widgets/image_pickers/big_image_picker.dart
@@ -0,0 +1,155 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:life_link/config/size_config.dart';
+import 'package:life_link/utils/assets.dart';
+import 'package:life_link/utils/colors.dart';
+
+class ImagePickerBigWidget extends StatelessWidget {
+  final String heading;
+  final String description;
+  final Function onPressed;
+  final PlatformFile? platformFile;
+  final String? imgUrl;
+
+  const ImagePickerBigWidget({
+    super.key,
+    required this.heading,
+    required this.description,
+    required this.onPressed,
+    required this.platformFile,
+    required this.imgUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: imgUrl != null && platformFile == null
+          ? MaterialButton(
+              onPressed: () async => onPressed(),
+              child: SizedBox(
+                width: double.infinity,
+                height: (SizeConfig.height20(context) * 8),
+                child: Stack(children: [
+                  Image.network(
+                    width: double.infinity,
+                    imgUrl!,
+                    fit: BoxFit.contain,
+                    loadingBuilder: (BuildContext context, Widget child,
+                        ImageChunkEvent? loadingProgress) {
+                      if (loadingProgress == null) {
+                        return child;
+                      }
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded /
+                                  loadingProgress.expectedTotalBytes!
+                              : null,
+                        ),
+                      );
+                    },
+                  ),
+                  Visibility(
+                    visible: imgUrl != null && platformFile == null,
+                    child: Container(
+                      height: (SizeConfig.height20(context) * 8),
+                      width: SizeConfig.width(context),
+                      color: Colors.transparent,
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: Colors.transparent.withOpacity(0.3)),
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "Edit Picture",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            color: whiteColor,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ]),
+              ),
+            )
+          : platformFile != null
+              ? SizedBox(
+                  width: (SizeConfig.width20(context) * 16.4),
+                  height: (SizeConfig.height20(context) * 8),
+                  child: Container(
+                    color: blackColor.withOpacity(0.6),
+                    child: MaterialButton(
+                      onPressed: () async => onPressed(),
+                      child: Image.file(
+                        File(platformFile!.path!),
+                        width: double.infinity,
+                      ),
+                    ),
+                  ),
+                )
+              : MaterialButton(
+                  onPressed: () async => onPressed(),
+                  child: Stack(
+                    alignment: AlignmentDirectional.center,
+                    children: [
+                      Image.asset(
+                        Assets.selectImgBigRectangle,
+                        fit: BoxFit.contain,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            top: SizeConfig.height15(context) * 5),
+                        child: Column(
+                          children: [
+                            Text(
+                              textAlign: TextAlign.center,
+                              heading,
+                              style: const TextStyle(
+                                color: appTextColor,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            SizedBox(
+                              width: (SizeConfig.height20(context) * 9) + 3,
+                              child: RichText(
+                                textAlign: TextAlign.center,
+                                text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: description,
+                                      style: const TextStyle(
+                                        color: greyColor,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                    const TextSpan(
+                                      text: '2 MB',
+                                      style: TextStyle(
+                                        color: greyColor,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+    );
+  }
+}
