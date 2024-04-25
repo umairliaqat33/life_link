abb9aebc7fb37b8da6c3fcbb9f9edaac208a25ea
Umair <umairliaqat552@gmail.com>
2024-04-25 15:41:34 +0500

    added files for hospital home screen

---
+++ lib/UI/screens/incoming_patients_screen/incoming_patients_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class IncomingPatientsScreen extends StatelessWidget {
+  const IncomingPatientsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("I am history screen"),
+      ),
+    );
+  }
+}
